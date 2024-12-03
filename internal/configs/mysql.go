79878c0b8909e9eee42973a40e2f472627cd8ca3
Sahrial Ardians <sahrialardians@gmail.com>
2024-12-03 09:26:43 +0700

    make connection database to mysql

--- internal/configs/mysql.go
+++ internal/configs/mysql.go
@@ -1,1 +1,16 @@
 package configs
+
+import (
+	"github.com/sahrialardians/rekeningku/internal/utils"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func DatabaseConnection() *gorm.DB {
+	dsn := "user:pass@tcp(127.0.0.1:3306)/rekeningku?charset=utf8mb4&parseTime=True&loc=Local"
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	utils.ErrorPanic(err)
+
+	return db
+}
