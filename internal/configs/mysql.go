32e44136d2d6960fbaff3ffe96484fbcf21f0c38
Sahrial Ardians <sahrialardians@gmail.com>
2024-12-07 21:40:19 +0700

    add log error

--- internal/configs/mysql.go
+++ internal/configs/mysql.go
@@ -1,16 +1,19 @@
 package configs
 
 import (
-	"github.com/sahrialardians/rekeningku/internal/utils"
+	"log"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func DatabaseConnection() *gorm.DB {
-	dsn := "user:pass@tcp(127.0.0.1:3306)/rekeningku?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "user:pass@tcp(127.0.0.1:3307)/rekeningku?charset=utf8mb4&parseTime=True&loc=Local"
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	utils.ErrorPanic(err)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
 
 	return db
 }
