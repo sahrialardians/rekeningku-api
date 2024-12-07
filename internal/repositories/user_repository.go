aaea6692377e21cd4c0d6c85c7ed778aedd46f82
Sahrial Ardians <sahrialardians@gmail.com>
2024-12-07 21:10:58 +0700

    create user interface

--- internal/repositories/user_repository.go
+++ internal/repositories/user_repository.go
@@ -1,1 +1,11 @@
 package repositories
+
+import "github.com/sahrialardians/rekeningku/internal/models"
+
+type UserRepository interface {
+	Save(user *models.User) (*models.User, error)
+	Update(user *models.User) error
+	Delete(usersId int) error
+	FindById(usersId int) (models.User, error)
+	FindByEmail(email string) (models.User, error)
+}
